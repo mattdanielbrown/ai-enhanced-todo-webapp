104a0d37c83290c04f678fdb674a9a9141f95f9d
Christian Liebel <christian.liebel@thinktecture.com>
2024-12-12 13:03:46 +0100

    Todo app implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {MatFabButton, MatIconButton} from '@angular/material/button';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatIcon} from '@angular/material/icon';
+import {MatList, MatListItem} from '@angular/material/list';
+import {NavComponent} from './nav/nav.component';
+import {type Todo} from './todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavComponent, MatFabButton, MatIcon, MatIconButton, MatList, MatListItem, MatCheckbox],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-ai';
+  protected readonly todos = signal<Todo[]>([
+    {text: 'Wash clothes', done: true},
+    {text: 'Wash my car', done: true},
+    {text: 'Pet the dog', done: false},
+  ]);
+
+  addTodo() {
+    const text = prompt('Enter a new todo.');
+    if (text == null) {
+      return;
+    }
+
+    this.todos.update(todos => [...todos, {text, done: false}]);
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todos.update(todos => todos.filter(t => t !== todo));
+  }
+
+  toggleTodo(todo: Todo) {
+    this.todos.update(todos => todos.map(t => t !== todo ? t : {...t, done: !t.done}));
+  }
 }
